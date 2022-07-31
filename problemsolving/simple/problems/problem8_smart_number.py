0a3356dbff2db87591052c1a71f3065e1709cc09
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-07-31 20:01:30 +0530

    Added new problems

--- problemsolving/simple/problems/problem8_smart_number.py
+++ problemsolving/simple/problems/problem8_smart_number.py
@@ -1,8 +1,26 @@
+"""
+A number is called a smart number if it has an odd number of factors.
+Given some numbers, find whether they are smart numbers or not.
+
+Debug the given function is_smart_number to correctly check if a given number is a smart number.
+Sample Input
+4
+1
+2
+7
+169
+Sample Output
+
+YES
+NO
+NO
+YES
+"""
 import math
 
 def is_smart_number(num):
     val = int(math.sqrt(num))
-    if val ** 2 == num:
+    if num / val == 1:
         return True
     return False
 
