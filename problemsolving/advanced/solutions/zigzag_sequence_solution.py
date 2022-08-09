392b02ef30af0b352d82e3f18f2681cd21e330f8
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-08-09 22:37:19 +0530

    Added problems and solutions

--- problemsolving/advanced/solutions/zigzag_sequence_solution.py
+++ problemsolving/advanced/solutions/zigzag_sequence_solution.py
@@ -1,22 +1,17 @@
 """
-Given an array of n distinct integers, transform the array into a zig zag sequence by permuting the array
-elements. A sequence will be called a zig zag sequence if the first k elements in the sequence are in
+Given an array of n distinct integers, transform the array into a zigzag sequence by permuting the array
+elements. A sequence will be called a zigzag sequence if the first k elements in the sequence are in
 increasing order and the last k elements are in decreasing order, where k=(n+1)/2.
-You need to find the lexicographically smallest zig zag sequence of the given array.
-
+You need to find lexicographically the smallest zigzag sequence of the given array.
 Example.
 a=[]2,3,5,1,4]
-
-Now if we permute the array as []1,4,5,3,2], the result is a zig zag sequence.
-
+Now if we permute the array as [1,4,5,3,2], the result is a zigzag sequence.
 Debug the given function findZigZagSequence to return the appropriate
-zig zag sequence for the given input array.
-
+zigzag sequence for the given input array.
 Sample Input
 1
 7
 1 2 3 4 5 6 7
-
 Sample Output
 1 2 3 7 6 5 4
 """
