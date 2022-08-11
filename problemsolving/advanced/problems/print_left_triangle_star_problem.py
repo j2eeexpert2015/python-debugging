112243c35d3ac5adba4da626fbf1e3189c985925
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-08-11 23:25:10 +0530

    Added problems and solutions

--- problemsolving/advanced/problems/print_left_triangle_star_problem.py
+++ problemsolving/advanced/problems/print_left_triangle_star_problem.py
@@ -1,3 +1,11 @@
+"""
+The code should print a left triangle star like the following
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
 def print_left_triangle_star_pattern(n):
     for i in range(0, n):
         for j in range(0, i + 1):
