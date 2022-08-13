11012cf910d3b095d753d267f9b873a786b4e49e
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2022-08-14 00:09:07 +0530

    Added new scenarios

--- problemsolving/advanced/problems/bubblesort/test_bubblesort.py
+++
@@ -1,5 +0,0 @@
-import insertion_sort_problem as st
-
-testList=[5,26,8,3,93,5,6,14,7,8]
-
-print(st.bubblesort(testList))
--- problemsolving/advanced/solutions/bubblesort/test_bubblesort.py
+++
@@ -1,5 +0,0 @@
-import bubblesort as st
-
-testList=[5,26,8,3,93,5,6,14,7,8]
-
-print(st.bubblesort(testList))
